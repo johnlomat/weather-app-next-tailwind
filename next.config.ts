b39dffbadc8b18813722643d08a4a5aa23b7548e
John Lomat <edwardlomat1503@gmail.com>
2025-03-06 21:52:10 +0800

    chore: update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cdn.weatherapi.com",
-        pathname: "/weather/**",
+        protocol: 'https',
+        hostname: 'cdn.weatherapi.com',
+        pathname: '/weather/**',
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
